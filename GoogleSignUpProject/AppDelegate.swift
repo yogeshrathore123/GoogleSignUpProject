d369a61b8ed957dca28554cf4fa3a899f27b0825
yogeshrathore123 <yogeshrathore251@gmail.com>
2019-04-18 18:35:27 +0530

    Update pod file and give support for Swift 5

--- GoogleSignUpProject/AppDelegate.swift
+++ GoogleSignUpProject/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     let defaults = UserDefaults.standard
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         // Initialize sign-in
